772cb5e7bb9b454554936af1631aa3caec0ad0d2
Matsvei Niaverau <matvej2002@gmail.com>
2023-01-01 13:41:45 +0100

    aston: Define AAPT configuration
    Change-Id: Ic3a241c34c9d6437aebc7007a82487e5e42069b3

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
 # Kernel
 KERNEL_PREBUILT_DIR := $(DEVICE_PATH)-kernel
 
