7574df94aa5af941e4ea5eb412d0cb275602fd81
snnbyyds <snnbyyds@gmail.com>
2024-07-18 21:10:34 +0800

    aston: Initial commit for new target
    Change-Id: Ifab2afbcac4f8347b8db3b2d1df2ec2488c9026c

---
+++ device.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/oneplus/aston/aston-vendor.mk)
